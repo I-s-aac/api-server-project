4fbc057b57cd53a4c6e076a621155cf370515781
runaround <runaroundontheground@gmail.com>
2025-01-10 17:47:15 -0700

    added comments and some other stuff to help me later

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = 3000;
 
 app.use(
   session({
-    secret: "very secret",
+    secret: "replace later with env variable",
     resave: false,
     saveUninitialized: false,
     cookie: { maxAge: 10 * 1000 },
@@ -23,9 +23,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
+// return a jwt token on successful authentication
 app.get("/getToken", (req, res) => {});
+
+// error handling
+console.log("replace this");
+
+// validate incoming jwts for protected routes
+console.log("replace this");
+
+// CRUD things, only accessable with a valid jwt, return an error message or a success message and the created/updated/deleted object
+// read and save these CRUD operations in some json file
+app.post("/cards/create", (req, res) => {});
+app.put("/cards/:id", (req, res) => {});
+app.delete("/cards/:id", (req, res) => {});
+
+// read stuff
+app.get("/cards/count", (req, res) => {});
+app.get("/cards/random", (req, res) => {});
 app.get("/cards?", (req, res) => {});
 
+// read stuff but special (go through properties of cards maybe?)
+app.get("/sets", (req, res) => {});
+app.get("/types", (req, res) => {});
+app.get("/rarities", (req, res) => {});
+
+// start server
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
