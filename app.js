c27f4df2cd76ee153fd241bbb6739216225bfaa7
runaround <runaroundontheground@gmail.com>
2025-01-13 07:30:17 -0700

    removed session things

--- app.js
+++ app.js
@@ -3,21 +3,11 @@ import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 import express from "express";
-import session from "express-session";
 import jwt from "jsonwebtoken";
 import { expressjwt } from "express-jwt";
 
 const app = express();
 const port = 3000;
-
-app.use(
-  session({
-    secret: "replace later with env variable",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 10 * 1000 },
-  })
-);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,14 +16,13 @@ app.use(express.static("public"));
 // return a jwt token on successful authentication
 app.get("/getToken", (req, res) => {});
 
-// error handling
-console.log("replace this");
-
 // validate incoming jwts for protected routes
 console.log("replace this");
 
-// CRUD things, only accessable with a valid jwt, return an error message or a success message and the created/updated/deleted object
-// read and save these CRUD operations in some json file
+// error handling
+console.log("replace this");
+
+// jwt protected read, update, delete operations, output to .json file, return the affected object
 app.post("/cards/create", (req, res) => {});
 app.put("/cards/:id", (req, res) => {});
 app.delete("/cards/:id", (req, res) => {});
